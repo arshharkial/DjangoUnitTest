2fbb90e1f3d5022739dbb241adc71bda74eaa9c7
arshharkial <theadsh99@gmail.com>
2023-06-30 17:51:01 +0530

    Updated tests

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import Testcase
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Ingredient
 from recipe.serializers import IngredientSerializer
+from django.urls import reverse
 
 INGREDIENTS_URL = reverse("recipe:ingredient-list")
 
